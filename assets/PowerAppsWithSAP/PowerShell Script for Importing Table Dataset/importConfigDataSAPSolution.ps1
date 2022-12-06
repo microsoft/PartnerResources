e85a12f9b1d476a7bea480d78e5f308d1fa1a017
Tad Thompson <66692710+tadthompson@users.noreply.github.com>
2022-12-06 07:13:06 -0600

    Add files via upload

---
+++ assets/PowerAppsWithSAP/PowerShell Script for Importing Table Dataset/importConfigDataSAPSolution.ps1
@@ -0,0 +1,25 @@
+﻿# capture parameters
+param( [Parameter(Mandatory=$true)] $url,
+       [Parameter(Mandatory=$true)] $configDataPath)
+
+# validate all required params exist
+# we aren't checking validity here; just existence
+if( ([string]::IsNullOrEmpty($url)) -or ([string]::IsNullOrEmpty($configDataPath)))
+{
+    write-host "One or more required parameters are missing";
+    exit;
+}
+
+# create and select authentication profile
+pac auth create --url $url --name TempProfile
+$authSelected = pac auth select --name TempProfile
+
+# store environment url for later use
+$authSelected[1] -match "\.*\s(?<Url>https:\/\/\S*)\s"
+$envUrl = $Matches.Url
+
+# import config data 
+pac data import --data $configDataPath --environment $url
+
+# clean up authentication profile
+pac auth delete --name TempProfile
